83f0d3a4a354b2167aa2e39c76d2b7170e766c3b
Mike Jones <michaeljones@vandals.uidaho.edu>
2014-06-05 22:04:09 -0700

    Chapter 21!

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
@@ -12,12 +12,14 @@ public class Crime {
 	private static final String JSON_SOLVED = "solved";
 	private static final String JSON_DATE = "date";
 	private static final String JSON_PHOTO = "photo";
+	private static final String JSON_SUSPECT = "suspect";
 	
 	private UUID mId;
 	private String mTitle;
 	private Date mDate;
 	private boolean mSolved;
 	private Photo mPhoto;
+	private String mSuspect;
 	
 	public Crime() {
 		// Generate unique identifier
@@ -32,6 +34,8 @@ public class Crime {
 		mDate = new Date(json.getLong(JSON_DATE));
 		if (json.has(JSON_PHOTO)) 
 			mPhoto = new Photo(json.getJSONObject(JSON_PHOTO));
+		if (json.has(JSON_SUSPECT))
+			mSuspect = json.getString(JSON_SUSPECT);
 	}
 	
 	public JSONObject toJSON() throws JSONException {
@@ -40,8 +44,10 @@ public class Crime {
 		json.put(JSON_TITLE, mTitle);
 		json.put(JSON_SOLVED, mSolved);
 		json.put(JSON_DATE, mDate.getTime());
-		if (mPhoto != null)
+		if (mPhoto != null) {
 			json.put(JSON_PHOTO, mPhoto.toJSON());
+			json.put(JSON_SUSPECT, mSuspect);
+		}
 		return json;
 	}
 	
@@ -84,5 +90,13 @@ public class Crime {
 	
 	public void setPhoto(Photo p) {
 		mPhoto = p;
+	}
+	
+	public String getSuspect() {
+		return mSuspect;
+	}
+	
+	public void setSuspect(String suspect) {
+		mSuspect = suspect;
 	}
 }
